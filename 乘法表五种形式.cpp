bb86b4f700dd82b73a4b2d39450220870308d7fa
earthbound1984 <91579482+earthbound1984@users.noreply.github.com>
2022-05-09 21:19:29 +0800

    Add files via upload

---
+++ 乘法表五种形式.cpp
@@ -0,0 +1,65 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+    int i, m, n;
+    printf("\n形式1\n");
+    for (i = 1; i <= 9; ++i)
+    {
+        for (m = 1; m <= 7 * (9 - i); ++m)
+        {
+            printf(" ");
+        }
+        for (n = 1; n <= i; ++n)
+        {
+            printf("%d*%d=%-2d ",i,n,i*n);
+        }
+        printf("\n");
+    }
+
+    printf("\n形式2\n");
+    for (i = 1; i <= 9; ++i)
+    {
+        for (n = 1; n <= i; ++n)
+        {
+            printf("%d*%d=%-2d ", i, n, i * n);
+        }
+        printf("\n");
+    }
+
+    printf("\n形式3\n");
+    for (i = 1; i <= 9; ++i)
+    {
+        for (n=i; n <= 9; ++n)
+        {
+            printf("%d*%d=%-2d ", i, n, i * n);
+        }
+        printf("\n");
+    }
+
+    printf("\n形式4\n");
+    for (i = 1; i <= 9; ++i)
+    {
+        for (m = 1; m <= 7 * (i - 1); ++m)
+        {
+            printf(" ");
+        }
+        for(n = i; n <= 9; ++n)
+        {
+            printf("%d*%d=%-2d ", i, n, i * n);
+        }
+        printf("\n");
+    }
+
+    printf("\n形式5\n");
+    for (i = 1; i <= 9; ++i)
+    {
+        for (n = 1; n <= 9; ++n)
+        {
+            printf("%d*%d=%-2d ", i, n, i * n);
+        }
+        printf("\n");
+    }
+}
+
